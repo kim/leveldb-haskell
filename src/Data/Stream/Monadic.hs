d6c4ef646603d9d749697317b8f52850651d72af
Kim Altintop <kim.altintop@gmail.com>
2014-09-27 23:41:11 +0200

    Aha: inline zip eagerly, so we only need the zipWith/unzip rule

--- src/Data/Stream/Monadic.hs
+++ src/Data/Stream/Monadic.hs
@@ -631,7 +631,7 @@ zip :: (Functor m, Applicative m, Monad m)
     -> Stream m b
     -> Stream m (a, b)
 zip = zipWith (,)
-{-# INLINE [0] zip #-}
+{-# INLINE zip #-}
 
 zipWith :: (Functor m, Applicative m, Monad m)
         => (a -> b -> c)
@@ -661,9 +661,6 @@ unzip :: (Functor m, Monad m) => Stream m (a, b) -> m ([a], [b])
 unzip = foldr (\(a,b) ~(as, bs) -> (a:as, b:bs)) ([], [])
 {-# INLINE [0] unzip #-}
 {-# RULES
-"zip/unzip fusion" forall a b.
-    unzip (zip a b) = (,) <$> toList a <*> toList b
-
 "zipWith/unzip fusion" forall f a b.
     unzip (zipWith f a b) = (,) <$> toList a <*> toList b
   #-}
