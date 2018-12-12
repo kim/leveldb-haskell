6027c394703b92cd56f3be30ca636df325ed0c51
Kim Altintop <kim.altintop@gmail.com>
2018-12-12 11:20:19 +0100

    Enable all

--- .ci/travis-install.sh
+++ .ci/travis-install.sh
@@ -32,6 +32,9 @@ if ! [ -x $HOME/.ghcup/bin/ghcup ]; then
 fi
 
 export PATH="$HOME/.cabal/bin:$HOME/.ghcup/bin:$PATH"
-ghcup set "$GHCVER" || ghcup install "$GHCVER"
-ghcup set "$GHCVER"
-ghcup install-cabal
+
+ghcup set "$GHCVER" || {
+  ghcup install "$GHCVER" && ghcup set "$GHCVER"
+}
+
+command -v cabal || ghcup install-cabal
